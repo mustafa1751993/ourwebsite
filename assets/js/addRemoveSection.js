4619cb345fdaa386bae7d84290d0e6b9b1b2d4af
mustafa1751993 <162787812+mustafa1751993@users.noreply.github.com>
2024-03-09 11:29:26 +0300

    20

--- assets/js/addRemoveSection.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function () {
-   if ($(this).scrollTop() > 315) {
-      $('#dynamic').addClass('neewww');
-      console.log('bob');
-   } else {
-      $('#dynamic').removeClass('neewww');
-   }
-});
